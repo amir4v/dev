0bc13855738d7e32e2d8b1e59708ddb4dd86fbb9
Amir <84547283+amir4v@users.noreply.github.com>
2022-12-13 22:37:32 +0330

    soft_delete completed.

---
+++ soft_delete/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.utils.timezone import now
+
+
+class SoftDeleteQuerySet(models.query.QuerySet):
+    def delete(self, sure=False):
+        if sure:
+            return super().delete()
+        else:
+            print('SOFT or HARD ?')
+    
+    def soft_delete(self):
+        return self.update(soft_delete_dt=now())
+    
+    def hard_delete(self):
+        return super().delete()
+    
+    def restore(self):
+        return self.update(soft_delete_dt=None)
+
+
+class SoftDeleteManager(models.Manager):
+    def get_queryset(self):
+        return SoftDeleteQuerySet(self.model, using=self._db).all().filter(soft_delete_dt=None)
+    
+    def ALL(self):
+        return super().get_queryset()
+
+
+class Model(models.Model):
+    soft_delete_dt = models.DateTimeField(null=True, default=None, blank=True)
+    objects = SoftDeleteManager()
+    
+    def delete(self, sure=False):
+        if sure:
+            return super().delete()
+        else:
+            print('SOFT or HARD ?')
+    
+    def soft_delete(self):
+        self.soft_delete_dt = now()
+        return self.save()
+    
+    def hard_delete(self):
+        return super().delete()
+    
+    def restore(self):
+        self.soft_delete_dt = None
+        return self.save()
+
+
+# ##################################################################################################
