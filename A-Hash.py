423b52a556cf5c47a46ace9213958217d3731a04
Amir <84547283+amir4v@users.noreply.github.com>
2023-01-20 19:31:12 +0330

    A-Hash

---
+++ A-Hash.py
@@ -0,0 +1,42 @@
+import string
+
+
+CHARS = string.ascii_lowercase
+
+BASE = len(CHARS)
+
+chars_weight = dict([
+    (s, i) for s, i in zip(CHARS, range(1, BASE+1))
+])
+
+reverse_chars_weight = dict([
+    (i, s) for s, i in zip(CHARS, range(1, BASE+1))
+])
+
+
+"""
+A function for Hash-Table
+And the output is reversible to the original string
+"""
+def a_hash(string):
+    string = string[::-1]
+    result = 0
+    for i, c in enumerate(string):
+        result += (BASE ** i) * chars_weight.get(c)
+    return result # f'{result:,}'
+
+
+def reverse_a_hash(digits):
+    result = ''
+    while digits > 0:
+        mod = digits % (BASE)
+        digits = digits // BASE
+        if mod == 0:
+            mod = BASE
+            digits -= 1
+        result = reverse_chars_weight.get(mod) + result
+    return result
+
+
+print(a_hash(input('String:>>>')))
+print(reverse_a_hash(int(input('Digits:>>>'))))
