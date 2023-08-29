8f13301ff775394a934958400c51d0cccd80936d
Amir <84547283+amir4v@users.noreply.github.com>
2023-08-29 14:07:00 +0330

    Update pre_installed_for_vps.sh

--- pre_installed_for_vps.sh
+++ pre_installed_for_vps.sh
@@ -1,12 +1,12 @@
 apt update
 apt upgrade -y
-apt install git
-apt install python3-pip
-apt install virtualenv
-apt install pipenv
-apt install python3-pipenv
-apt install apache2
-apt install nginx
-apt install wget
-apt install curl
-apt install sqlite3
+apt install git -y
+apt install python3-pip -y
+apt install virtualenv -y
+apt install pipenv -y
+apt install python3-pipenv -y
+apt install apache2 -y
+apt install nginx -y
+apt install wget -y
+apt install curl -y
+apt install sqlite3 -y
