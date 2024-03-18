8d22bb78211db3bf1dbb1da28986a7d2c79388eb
Amir <84547283+amir4v@users.noreply.github.com>
2024-03-18 08:43:07 +0330

    cleaning and fix the error about cookies which needed a special cookie called session_id

--- mktb/cookies.py
+++ mktb/cookies.py
@@ -1,1 +1,28 @@
-cookies = {}
+# document.cookie
+
+# "sessionid": "...", # *******************************************************
+
+cookies = {
+# "beta_tester_conditional": "...",
+# "analytics_token": "...",
+# "beta_tester_expiration": "...",
+# "_gcl_au": "...",
+# "_ga_T6428MCTQQ": "...",
+# "ribbon_expire": "...",
+"sessionid": "...",
+# "csrftoken": "...",
+# "csrftoken": "...",
+"user-role": "...",
+"_pk_ref.2.c515": "...",
+"_pk_ses.2.c515": "...",
+# "check_phone_number": "...",
+"_gid": "...",
+"_ga": "...",
+"_ga_53QNTVNLXS": "...",
+# "analytics_session_token": "...",
+# "yektanet_session_last_activity": "...",
+# "_yngt_iframe": "...",
+# "_yngt": "...",
+"_ga_GMGYXM0TZF": "...",
+"_pk_id.2.c515": "...",
+}
--- mktb/urls.py
+++ mktb/urls.py
@@ -1,7 +1,15 @@
 from until import where
 
-links = []
+links = [
+]
 
+"""
+_class = "flex p-10 border-0 border-b-hawkes-blue border-solid bg-white justify-between rounded-none cursor-pointer";
+as = document.getElementsByClassName(_class);
+for(i=0; i < as.length; i++){
+    console.log(as[i].href);
+}
+"""
 
 def urls():
     _where = where() + 1
