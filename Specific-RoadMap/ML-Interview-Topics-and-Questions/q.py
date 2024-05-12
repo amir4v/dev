8d511a85bb5cbf45b4423e2d646df94d118f0acd
Amir <amir4vx@gmail.com>
2024-05-12 17:12:40 +0330

    summeries of topics

--- Specific-RoadMap/ML-Interview-Topics-and-Questions/q.py
+++ Specific-RoadMap/ML-Interview-Topics-and-Questions/q.py
@@ -1,10 +1,12 @@
 import random
 
-q_type = int(input('InterviewQ/1 or QinT/2'))
+q_type = int(input('InterviewQ/1 or QinT/2 SoftSkills/3:'))
 if q_type == 1:
     filename = './InterviewQ.txt'
-else:
+elif q_type == 2:
     filename = './QinT.txt'
+elif q_type == 3:
+    filename = './SoftSkills.txt'
 
 while 1:
     lines = open(filename, 'rt', encoding='utf-8').read().split('\n')
