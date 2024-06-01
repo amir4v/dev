d4dd85fc78ee9da7d6c03e9ce785bd31e3ca6247
Amir <amir4vx@gmail.com>
2024-06-01 08:50:05 +0330

    deep learning codes from sentdex

---
+++ Specific-RoadMap/ML-Interview-Topics-and-Questions/Interview-SoftSkills-and/q.py
@@ -0,0 +1,14 @@
+import random
+
+q_type = int(input('InterviewQ/1 or InterviewQ-Focused/2 SoftSkills/3:'))
+if q_type == 1:
+    filename = './InterviewQ.txt'
+elif q_type == 2:
+    filename = './InterviewQ-Focused.txt'
+elif q_type == 3:
+    filename = './SoftSkills.txt'
+
+while 1:
+    lines = open(filename, 'rt', encoding='utf-8').read().split('\n')
+    print(lines[random.randint(0, len(lines))])
+    input('>>>')
